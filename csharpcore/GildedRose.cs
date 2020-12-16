e543e51e0b1ed91e5e320433c9a0d7268c1e2560
hkirk <henrikkirk@gmail.com>
2020-12-16 21:40:47 +0100

    Added tests

--- csharpcore/GildedRose.cs
+++ csharpcore/GildedRose.cs
@@ -4,6 +4,10 @@ namespace csharpcore
 {
     public class GildedRose
     {
+        public static string SulfurasHandOfRagnaros = "Sulfuras, Hand of Ragnaros";
+        public static string BackstagePassesToATafkal80etcConcert = "Backstage passes to a TAFKAL80ETC concert";
+        public static string AgedBrie = "Aged Brie";
+        
         IList<Item> Items;
         public GildedRose(IList<Item> Items)
         {
@@ -14,11 +18,12 @@ namespace csharpcore
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                
+                if (Items[i].Name != AgedBrie && Items[i].Name != BackstagePassesToATafkal80etcConcert)
                 {
                     if (Items[i].Quality > 0)
                     {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                        if (Items[i].Name != SulfurasHandOfRagnaros)
                         {
                             Items[i].Quality = Items[i].Quality - 1;
                         }
@@ -30,7 +35,7 @@ namespace csharpcore
                     {
                         Items[i].Quality = Items[i].Quality + 1;
 
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (Items[i].Name == BackstagePassesToATafkal80etcConcert)
                         {
                             if (Items[i].SellIn < 11)
                             {
@@ -51,20 +56,20 @@ namespace csharpcore
                     }
                 }
 
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                if (Items[i].Name != SulfurasHandOfRagnaros)
                 {
                     Items[i].SellIn = Items[i].SellIn - 1;
                 }
 
                 if (Items[i].SellIn < 0)
                 {
-                    if (Items[i].Name != "Aged Brie")
+                    if (Items[i].Name != AgedBrie)
                     {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                        if (Items[i].Name != BackstagePassesToATafkal80etcConcert)
                         {
                             if (Items[i].Quality > 0)
                             {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                                if (Items[i].Name != SulfurasHandOfRagnaros)
                                 {
                                     Items[i].Quality = Items[i].Quality - 1;
                                 }
--- csharpcore/GildedRoseTest.cs
+++ csharpcore/GildedRoseTest.cs
@@ -8,10 +8,157 @@ namespace csharpcore
         [Fact]
         public void foo()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            IList<Item> Items = new List<Item> {new Item {Name = "foo", SellIn = 0, Quality = 0}};
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
             Assert.Equal("foo", Items[0].Name);
         }
+
+        [Fact]
+        public void decreaseQualityOfFoo()
+        {
+            IList<Item> Items = new List<Item> {new Item {Name = "foo", SellIn = 1, Quality = 2}};
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Assert.Equal(1, Items[0].Quality);
+        }
+
+        [Fact]
+        public void decreaseQualityOfFooWithZeroSellIn()
+        {
+            IList<Item> Items = new List<Item> {new Item {Name = "foo", SellIn = 0, Quality = 2}};
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Assert.Equal(0, Items[0].Quality);
+        }
+
+        [Fact]
+        public void sulfurasDoNotChangeQuality()
+        {
+            IList<Item> Items = new List<Item>
+            {
+                new Item
+                    {Name = GildedRose.SulfurasHandOfRagnaros, SellIn = 0, Quality = 2}
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Assert.Equal(2, Items[0].Quality);
+        }
+
+        [Fact]
+        public void increaseQualityOfAgedBrie()
+        {
+            IList<Item> Items = new List<Item>
+            {
+                new Item
+                    {Name = GildedRose.AgedBrie, SellIn = 1, Quality = 2}
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Assert.Equal(3, Items[0].Quality);
+        }
+
+        [Fact]
+        public void inceaseValueOfAgedBrieWithZeroSellIn()
+        {
+            IList<Item> Items = new List<Item>
+            {
+                new Item
+                    {Name = GildedRose.AgedBrie, SellIn = 0, Quality = 2}
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Assert.Equal(4, Items[0].Quality);
+        }
+
+        [Fact]
+        public void increaseValueOfBackstagePasses()
+        {
+            IList<Item> Items = new List<Item>
+            {
+                new Item
+                    {Name = GildedRose.BackstagePassesToATafkal80etcConcert, SellIn = 11, Quality = 2}
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Assert.Equal(3, Items[0].Quality);
+        }
+
+        [Fact]
+        public void increaseValueOfBackstagePasses10SellIn()
+        {
+            IList<Item> Items = new List<Item>
+            {
+                new Item
+                    {Name = GildedRose.BackstagePassesToATafkal80etcConcert, SellIn = 10, Quality = 2}
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Assert.Equal(4, Items[0].Quality);
+        }
+
+        [Fact]
+        public void increaseValueOfBackstagePasses5SellIn()
+        {
+            IList<Item> Items = new List<Item>
+            {
+                new Item
+                    {Name = GildedRose.BackstagePassesToATafkal80etcConcert, SellIn = 5, Quality = 2}
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Assert.Equal(5, Items[0].Quality);
+        }
+
+        [Fact]
+        public void BackstagePassesQualityZeroWhenSellInIsPassed() {
+
+            IList<Item> Items = new List<Item>
+            {
+                new Item
+                    {Name = GildedRose.BackstagePassesToATafkal80etcConcert, SellIn = 0, Quality = 20}
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Assert.Equal(0, Items[0].Quality);
+        }
+
+        [Fact]
+        public void SellInDecreases()
+        {
+            IList<Item> Items = new List<Item>
+            {
+                new Item
+                    {Name = GildedRose.BackstagePassesToATafkal80etcConcert, SellIn = 0, Quality = 20}
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Assert.Equal(-1, Items[0].SellIn);
+        }
+        
+        [Fact]
+        public void SellInIgnoreForSulfuras()
+        {
+            IList<Item> Items = new List<Item>
+            {
+                new Item
+                    {Name = GildedRose.SulfurasHandOfRagnaros, SellIn = 10, Quality = 20}
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+
+            Assert.Equal(10, Items[0].SellIn);
+        }
+
     }
 }
